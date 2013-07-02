f548a0921cc66ad675bab05e337ca73dd21375b4
anon anon <delgado.raymond@gmail.com>
2013-07-01 22:24:52 -0400

    progress

--- script2.js
+++ script2.js
@@ -13,23 +13,41 @@ $(document).ready(function(){
     return questions;
   })();
 
-  console.log("there are " + questions.length + " questions right now in 'questions.json'");
-  //console.log(questions[0].correct_answer);
+  //console.log("there are " + questions.length + " questions right now in 'questions.json'");
 
+  //holds the answers per question
   var answer = [];
+  //fills the array 'answer' with each answer per question
   var one_at_a_time = function(){
     for (var i = 0; i < questions.length; i++) {
-      //console.log(questions[i].correct_answer)
-      answer[i] = questions[i].correct_answer;
+        answer[i] = questions[i].correct_answer;
+    }
+  }
+
+  //adds a radio button per multiple choice option
+  var multiple_choice = function(target,question_order){
+    for (var i = 0; i < questions[question_order].options.length; i++) {
+       $('<input type="radio" name="options">' + questions[i].options[i] + '<br>').appendTo(target);
     }
   }
 
   one_at_a_time();
-  console.log("Reading from the 'questions.json' file: Question 1 is " + "'" + questions[0].question + "'" + ". Answer 1 is " + "'" + answer[0] + "'" + ".");
-//users
 
-$('#questions').append("<h1>test</h1>");
+  //dynamically draw the questions and log the answers (correct or wrong),
+  //then add a back and forth functionality
 
+  //save div as it was to be used and reused
+  var div_clone = $("#questions").html();
 
+  var quiz = function() {
+    for (var i = 0; i < questions.length; i++ ) {
+      $('#part').append(questions[i].part);
+      $('question').append(questions[i].question);
+      multiple_choice('#radioform',0);
+      $("#questions").html(div_clone);
+    }
+  }
+
+  quiz();
 
 });
