21db6bdc71b08e45a52ddade33300705c6959761
anon anon <delgado.raymond@gmail.com>
2013-07-02 23:05:37 -0400

    objects

--- script2.js
+++ script2.js
@@ -13,16 +13,33 @@ $(document).ready(function(){
     return questions;
   })();
 
-  //console.log("there are " + questions.length + " questions right now in 'questions.json'");
+  $('#next').hide();
+  $('#back').hide();
+  $('#restart').hide();
+
+  $('#restart').click(function() {
+    $('#next').hide();
+    $('#back').hide();
+    $('#restart').hide();
+    $('#start').show();
+  });
+
+    //save div as it was to be used and reused
+  var div_clone = $("#questions").html();
+  // $("#questions").html(div_clone); //this returns the div to original state
+
+  console.log("there are " + questions.length + " questions right now in 'questions.json'");
 
   //holds the answers per question
   var answer = [];
   //fills the array 'answer' with each answer per question
-  var one_at_a_time = function(){
+  var answers = function(){
     for (var i = 0; i < questions.length; i++) {
         answer[i] = questions[i].correct_answer;
     }
   }
+
+  answers();
 
   //adds a radio button per multiple choice option
   var multiple_choice = function(target,question_order){
@@ -31,23 +48,38 @@ $(document).ready(function(){
     }
   }
 
-  one_at_a_time();
-
   //dynamically draw the questions and log the answers (correct or wrong),
-  //then add a back and forth functionality
+  //then add a back and forth functionality by traversing the json array object
 
-  //save div as it was to be used and reused
-  var div_clone = $("#questions").html();
-
-  var quiz = function() {
-    for (var i = 0; i < questions.length; i++ ) {
-      $('#part').append(questions[i].part);
-      $('question').append(questions[i].question);
-      multiple_choice('#radioform',0);
-      $("#questions").html(div_clone);
-    }
+  function Quiz(number) {
+    this.question = questions[number].question;
+    this.answer = questions[number].correct_answer;
+    this.answered = false;
+    this.number = number;
   }
 
-  quiz();
+  Quiz.prototype.ask = function() {
+
+  }
+
+  qna = new Quiz(1);
+
+  $('#start').click(function() {
+    $('#next').show();
+    $('#back').show();
+    $('#restart').show();
+    $('#start').hide();
+
+    $('#question').append(qna.question);
+    multiple_choice('#radioform',0);
+  });
+
+  console.log(qna.question + qna.answer + qna.answered);
+
+  //
+  //$('#back')click(function() {
+  //
+  //})
+
 
 });
